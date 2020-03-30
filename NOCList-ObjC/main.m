5233ec3406637148c5a0ceeb26f781cd3703ad23
Craig Swanson <cswanson02@me.com>
2020-03-30 15:29:24 -0500

    Initial Commit

---
+++ NOCList-ObjC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NOCList-ObjC
+//
+//  Created by Craig Swanson on 3/30/20.
+//  Copyright © 2020 craigswanson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
