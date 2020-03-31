7ef3cab40a9950fd720cbf8560eb987330aef47e
Craig Swanson <cswanson02@me.com>
2020-03-30 22:54:17 -0500

    Created objects for each agent

--- NOCList-ObjC/main.m
+++ NOCList-ObjC/main.m
@@ -7,11 +7,35 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "LSIAgent.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        LSIAgent *eHunt = [[LSIAgent alloc] initWithString:@"Ethan Hunt" realName:@"Tom Cruise" accessLevel:8 compromised:NO];
+        LSIAgent *jPhelps = [[LSIAgent alloc] initWithString:@"Jim Phelps" realName:@"Jon Voight" accessLevel:9 compromised:YES];
+        LSIAgent *cPhelps = [[LSIAgent alloc] initWithString:@"Claire Phelps" realName:@"Emmanuelle Beart" accessLevel:5 compromised:NO];
+        LSIAgent *eKittridge = [[LSIAgent alloc] initWithString:@"Eugene Kittridge" realName:@"Henry Czerny" accessLevel:10 compromised:YES];
+        LSIAgent *fKrieger = [[LSIAgent alloc] initWithString:@"Franz Krieger" realName:@"Jean Reno" accessLevel:4 compromised:NO];
+        LSIAgent *lStickell = [[LSIAgent alloc] initWithString:@"Luther Stickell" realName:@"Ving Rhames" accessLevel:4 compromised:NO];
+        LSIAgent *sDavies = [[LSIAgent alloc] initWithString:@"Sarah Davies" realName:@"Kristin Scott Thomas" accessLevel:5 compromised:YES];
+        LSIAgent *mRotGrab = [[LSIAgent alloc] initWithString:@"Max RotGrab" realName:@"Vanessa Redgrave" accessLevel:4 compromised:NO];
+        LSIAgent *hWilliams = [[LSIAgent alloc] initWithString:@"Hannah Williams" realName:@"Ingeborga Dapkūnaitė" accessLevel:5 compromised:YES];
+        LSIAgent *jHarmon = [[LSIAgent alloc] initWithString:@"Jack Harmon" realName:@"Emilio Estevez" accessLevel:6 compromised:YES];
+        LSIAgent *fBarnes = [[LSIAgent alloc] initWithString:@"Frank Barnes" realName:@"Dale Dye" accessLevel:9 compromised:NO];
+        
+        NSArray *nocList = @[eHunt,
+                             jPhelps,
+                             cPhelps,
+                             eKittridge,
+                             fKrieger,
+                             lStickell,
+                             sDavies,
+                             mRotGrab,
+                             hWilliams,
+                             jHarmon,
+                             fBarnes];
+        
     }
     return 0;
 }
